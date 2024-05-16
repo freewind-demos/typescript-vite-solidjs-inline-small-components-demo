d15627c41b82327489d96d45532d49ec6d3cb5da
Freewind <nowindlee@gmail.com>
2024-05-16 13:33:54 +0800

    typescript-vite-solidjs-inline-small-components-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript Vite SolidJS Demo
+TypeScript Vite SolidJS Inline Small Components Demo
 ===========================
+
+solidjs可以在局部定义signal供使用，不像react那样必须放在body最上面或者另外定义一个小component。
 
 ```
 npm install
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,11 +1,19 @@
-import {createSignal} from 'solid-js';
-
-const [name, setName] = createSignal<string>('solidjs')
+import { createSignal } from 'solid-js';
 
 export const Hello = () => {
-
   return <div>
-    <h1>Hello, {name()}</h1>
-    <input type="text" value={name()} onInput={(event) => setName(event.currentTarget.value)}/>
+    {
+      doExpr(() => {
+        const [name, setName] = createSignal<string>('solidjs')
+        return <>
+          <h1>Hello, {name()}</h1>
+          <input type="text" value={name()} onInput={(event) => setName(event.currentTarget.value)} />
+        </>
+      })
+    }
   </div>
+}
+
+function doExpr<T>(fn: () => T): T {
+  return fn();
 }
